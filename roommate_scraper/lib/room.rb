7a6a9ac90d7c752907877eb5ca5b385e109ab3b6
Adam S <adam.sarli@gmail.com>
2019-04-12 20:15:37 +0000

    self.all returns array of re-instantiated Room instances from DB

--- roommate_scraper/lib/room.rb
+++ roommate_scraper/lib/room.rb
@@ -2,19 +2,30 @@ class Room
   attr_accessor :id, :time, :price, :title, :url 
   @@all = []
   
-  def initialize(attributes)
+  def initialize(attributes={})
     attributes.each do |key, value|
       self.send("#{key}=",value)
     end 
-    @@all << self 
   end 
   
   def self.all
-    if @@all == [] 
-      scrape_rooms
-    end 
-    @@all 
+    sql = %{
+      SELECT * FROM rooms
+    }
+    DB[:conn].execute(sql).map{|row| new_from_db(row)}
   end 
+  
+  def self.new_from_db(row)
+    room = self.new 
+    room.id = row[0]
+    room.time = row[1]
+    room.price = row[2]
+    room.title = row[3]
+    room.url = row[4] 
+    
+    room
+  end 
+  
   
   def self.scrape_rooms 
     RoomScraper.new.scrape.each do |attributes_hash|

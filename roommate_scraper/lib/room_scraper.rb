4546e4c813e893a5b1b1b2a414e0015bf5ec0529
Adam S <adam.sarli@gmail.com>
2019-04-12 17:32:57 +0000

    set up Rakefile tasks

---
+++ roommate_scraper/lib/room_scraper.rb
@@ -0,0 +1,12 @@
+class RoomScraper 
+  attr_accessor :path 
+  
+  def initialize(path="https://newyork.craigslist.org/search/roo")
+    @path = path 
+  end 
+  
+  def update_rooms 
+    puts "hi!"
+  end 
+  
+end 

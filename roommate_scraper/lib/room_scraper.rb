96d2490b4fba99db89fc1934fa89dcf4efb6e2b0
Adam S <adam.sarli@gmail.com>
2019-04-12 17:53:06 +0000

    scraped nokogiri commands

--- roommate_scraper/lib/room_scraper.rb
+++ roommate_scraper/lib/room_scraper.rb
@@ -1,9 +1,29 @@
+require 'open-uri'
+
 class RoomScraper 
-  attr_accessor :path 
+  attr_accessor :path, :doc  
   
   def initialize(path="https://newyork.craigslist.org/search/roo")
     @path = path 
+    html = open(@path)
+    @doc = Nokogiri::HTML(html)
   end 
+  
+  def scrape 
+    # time = doc.css(".result-row")[0].css("time").text
+    # price = doc.css(".result-row")[0].css("a span")[0].text
+    # title = doc.css(".result-row")[0].css("a.result-title.hdrlnk").text
+    
+    doc.css(".result-row").each do |listing|
+    time = listing.css("time").text
+    price = listing.css("a span")[0].text
+    title = listing.css("a.result-title.hdrlnk").text
+    binding.pry 
+    end 
+    
+    
+  end 
+  
   
   def update_rooms 
     puts "hi!"
